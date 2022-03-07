7037d3c62a762a78c73888ca35c8b7e2a9703610
Daniel Oravec <danoravec@gmail.com>
2022-03-07 19:34:58 +0100

    Remove redundant context variables
    * They were only needed in the original
      application, but are not used in this one

--- ledger-app-fio/src/signTransaction.h
+++ ledger-app-fio/src/signTransaction.h
@@ -19,32 +19,7 @@ typedef struct
 	sha_256_context_t txHashContext;
 	sha_256_context_t integrityHashContext;
 
-	char actionValidationActor[NAME_STRING_MAX_LENGTH];
-
 	int ui_step;
-
-	// The following data is not needed at once.
-	// to be used in HEADER step
-	uint32_t expiration;
-	uint16_t refBlockNum;
-	uint32_t refBlockPrefix;
-
-	// only used in ACTION HEADER step
-	action_type_t action_type;
-
-	// only used in ACTION_AUTHORIZATION steps
-	char actionValidationPermission[NAME_STRING_MAX_LENGTH];
-
-	// only used in ACTION_DATA step
-	char *pubkey;
-	uint64_t amount;
-	uint64_t maxFee;
-	char actionDataActor[NAME_STRING_MAX_LENGTH];
-	char *tpid;
-
-	// only used in WITNESS step
-	bip44_path_t wittnessPath;
-	public_key_t wittnessPathPubkey;
 
 	char *headerBuf;
 	char *bodyBuf;
@@ -58,11 +33,11 @@ typedef struct
 	uint64_t expectedSectionLength[MAX_NESTED_COUNTED_SECTIONS + 1];
 	uint64_t currSectionLength[MAX_NESTED_COUNTED_SECTIONS + 1]; // Will be increased by SEND_DATA
 
-	uint8_t forIterationsCnt[MAX_FOR_DEPTH + 1]; // Current number for iterations
+	uint8_t forIterationsCnt[MAX_FOR_DEPTH + 1]; // Current number of for iterations
 	uint8_t forMinIterations[MAX_FOR_DEPTH + 1]; // Min allowed number of iterations
 	uint8_t forMaxIterations[MAX_FOR_DEPTH + 1]; // Max allowed number of iterations
+	uint8_t forLevel;							 // Depth of the current for (nested), 0 if we are not inside a for
 
-	uint8_t forLevel;					  // Depth of the current for (nested), 0 if we are not inside a for
 	uint8_t intHashes[MAX_FOR_DEPTH][32]; // List of integrity hashes of fors with lower level
 	uint8_t allowedIterationHashesHash[MAX_FOR_DEPTH][32];
 
